6399d99c3743aa29ba52e356cd93e75bf81fee1b
Cristian Iosif <cristianiosif@me.com>
2020-04-23 21:14:27 +0300

    Changes color schema and adds scroll to top

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -6,7 +6,7 @@
 			<h2 class="uppercase tracking-wider text-orange-500 text-lg font-semibold">{{ __('Popular Movies') }}</h2>
 
 			<div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-8">
-				@foreach ($popularMovies as $movie)
+				@foreach($popularMovies as $movie)
 					<x-movie-card :movie="$movie" />
 				@endforeach
 			</div>
